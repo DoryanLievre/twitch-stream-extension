a3d1974e504fdcecad89a6a3a466c8c320e2e507
Doryan L <dlievre.dev@gmail.com>
2022-12-12 21:54:40 +0100

    feat(get-streamer-info): convert js to typeScript

---
+++ Backend/get-streamer-info.ts
@@ -0,0 +1,25 @@
+const url = `https://api.twitch.tv/helix/streams?user_id=${userId}`;
+const headers = {
+    'Authorization': `Bearer ${token}`,
+    'Client-Id': ``,
+};
+
+const channelInfo = document.getElementById('info') as HTMLElement;
+
+const checker = function (json: any) {
+    if (json.data.length > 0) {
+        channelInfo.innerText = "I'm awake come to my place precious little gem ~"
+    } else {
+        channelInfo.innerText = "I'm a dangerous noodle BUT I'm sleeping right now ~"
+    }
+}
+
+function fetchTwitchApi(url: string, headers: any, checker: (json: any) => void) {
+    fetch(url, {
+        headers: headers
+    }).then((response) => {
+        return response.json();
+    }).then((json) => checker(json));
+}
+
+fetchTwitchApi(url, headers, checker);
