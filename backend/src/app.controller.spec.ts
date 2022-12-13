1ece743d73ec008bb90ce6924b4c99553459ec3c
JonahR1 <jonah_reborn@hotmail.fr>
2022-12-13 18:33:17 +0100

    feat(backend): /health path returns Alive! when server running

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Alive!"', () => {
+      expect(appController.getHealth()).toBe('Alive!');
     });
   });
 });
